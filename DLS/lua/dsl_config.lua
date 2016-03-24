e904351ee16e730a7af79f1130e84f035c7be0a9
Dilly <DillyRek@users.noreply.github.com>
2016-03-24 09:03:40 -0700

    Update dsl_config.lua

--- DLS/lua/dsl_config.lua
+++ DLS/lua/dsl_config.lua
@@ -1,18 +1,1 @@
-DLSConfig = {}
-DLSConfig.DevMode = false
-
-
--- Do not remove this
--- Created by: ♕ Dilly ✄
--- Steamid: http://steamcommunity.com/id/DillyRektChu/
-
-DLSConfig.PriceForTicket = 100
-
-DLSConfig.RewardMatch1 = 1000 -- The amount of money won for matching 1 number
-DLSConfig.RewardMatch2 = 5000 -- The amount of money won for matching 2 numbers
-DLSConfig.RewardMatch3 = 10000 -- The amount of money won for matching 3 numbers
-
-DLSConfig.CoolDown = 120 -- The amount of time (in sec) a player has to wait to buy another ticket
-
-DLSConfig.AutoBanExploiters = true -- THIS ONLY WORKS FOR ULX IF YOU DO NOT USE ULX DO NOT USE THIS
--- If a player tries to use exploits with this addon he/she will be auto banned (ulx banid <players id> Attempting to use exploits on the lottery addon)
+DLS
