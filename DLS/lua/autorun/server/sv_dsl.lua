52378498d39ee4b371261b3fb398f36a3eef0bc0
Dilly <DillyRek@users.noreply.github.com>
2016-03-24 08:40:49 -0700

    Update sv_dsl.lua

--- DLS/lua/autorun/server/sv_dsl.lua
+++ DLS/lua/autorun/server/sv_dsl.lua
@@ -1,18 +1,18 @@
-util.AddNetworkString("LMMLOpenTicket")
-util.AddNetworkString("LMMLRemoveMoney")
-util.AddNetworkString("LMMLRewards")
-util.AddNetworkString("LMMLRewardsText")
-util.AddNetworkString("LMMLCoolDownText")
+util.AddNetworkString("DLSOpenTicket")
+util.AddNetworkString("DLSRemoveMoney")
+util.AddNetworkString("DLSRewards")
+util.AddNetworkString("DLSRewardsText")
+util.AddNetworkString("DLSCoolDownText")
 
-function LMMLPutUserOnCoolDown(ply)
-	ply:SetNWBool("LMMLCoolDown", true)
-	timer.Create("LMMLCoolDownFor"..ply:SteamID64(), LMMLConfig.CoolDown, 1, function()
-		ply:SetNWBool("LMMLCoolDown", false)
+function DLSPutUserOnCoolDown(ply)
+	ply:SetNWBool("DLSCoolDown", true)
+	timer.Create("DLSCoolDownFor"..ply:SteamID64(), DLSConfig.CoolDown, 1, function()
+		ply:SetNWBool("DLSCoolDown", false)
 	end	)
 end
 
-function LMMLCheckCoolDown(ply)
-	return ply:GetNWBool("LMMLCoolDown", false)
+function DLSCheckCoolDown(ply)
+	return ply:GetNWBool("DLSCoolDown", false)
 end
 
 function SendLotteryTicket(ply)
@@ -24,7 +24,7 @@ function SendLotteryTicket(ply)
 	local answer2 = math.random(1, 3)
 	local answer3 = math.random(1, 3)
 	
-	net.Start("LMMLOpenTicket")
+	net.Start("DLSOpenTicket")
 		net.WriteFloat(num1)
 		net.WriteFloat(num2)
 		net.WriteFloat(num3)
@@ -33,29 +33,29 @@ function SendLotteryTicket(ply)
 		net.WriteFloat(answer3)
 	net.Send(ply)
 	
-	net.Receive("LMMLRemoveMoney", function( len, theply )
+	net.Receive("DLSRemoveMoney", function( len, theply )
 		if theply ~= ply then
-			if LMMLConfig.AutoBanExploiters then
+			if DLSConfig.AutoBanExploiters then
 				RunConsoleCommand("ulx", "banid", ply:SteamID(), "0", "Attempting to use exploits on the lottery addon")
 			end
 			return
 		else
-			if theply:getDarkRPVar("money") > LMMLConfig.PriceForTicket then
-				theply:addMoney(-LMMLConfig.PriceForTicket)
+			if theply:getDarkRPVar("money") > DLSConfig.PriceForTicket then
+				theply:addMoney(-DLSConfig.PriceForTicket)
 			else
 				theply:ChatPrint("You do not have enough money!")
 			end
 		end
 	end)
 	
-	net.Receive("LMMLRewards", function( len, ply )
+	net.Receive("DLSRewards", function( len, ply )
 		
 		local winners = 0
 		local clnum1, clnum2, clnum3 = net.ReadFloat(), net.ReadFloat(), net.ReadFloat()
 		local clans1, clans2, clans3 = net.ReadFloat(), net.ReadFloat(), net.ReadFloat()
 		
 		if ( clnum1 ~= num1 or clnum2 ~= num2 or clnum3 ~= num3 or clans1 ~= answer1 or clans2 ~= answer2 or clans3 ~= answer3 ) then
-			if LMMLConfig.AutoBanExploiters then
+			if DLSConfig.AutoBanExploiters then
 				RunConsoleCommand("ulx", "banid", ply:SteamID(), "0", "Attempting to use exploits on the lottery addon")
 			end
 			return
@@ -66,33 +66,33 @@ function SendLotteryTicket(ply)
 		if clnum3 == answer3 then winners = winners + 1 end
 		
 		if winners == 1 then
-			ply:addpoints(LMMLConfig.RewardMatch1)
+			ply:addpoints(DLSConfig.RewardMatch1)
 		elseif winners == 2 then
-			ply:addpoints(LMMLConfig.RewardMatch2)
+			ply:addpoints(DLSConfig.RewardMatch2)
 		elseif winners == 3 then
-			ply:addpoints(LMMLConfig.RewardMatch3)
+			ply:addpoints(DLSConfig.RewardMatch3)
 		end			
 		
-		net.Start("LMMLRewardsText")
+		net.Start("DLSRewardsText")
 			net.WriteFloat(winners)
 		net.Send(ply)
 		
-		LMMLPutUserOnCoolDown(ply)
+		DLSPutUserOnCoolDown(ply)
 		
 	end )
 	
 end
 
-function LMMLOpenMenu(ply, text)
+function DLSOpenMenu(ply, text)
 	local text = string.lower(text)
 	if ( string.sub( string.lower(text), 0, 7) == "!lottery" ) then
-		if not LMMLCheckCoolDown(ply) then
+		if not DLSCheckCoolDown(ply) then
 			SendLotteryTicket(ply)
 		else
-			net.Start("LMMLCoolDownText")
+			net.Start("DLSCoolDownText")
 			net.Send(ply)
 		end
 		return ''
 	end
 end 
-hook.Add("PlayerSay", "LMMLOpenMenu", LMMLOpenMenu)
+hook.Add("PlayerSay", "DLSOpenMenu", DLSOpenMenu)
